0f5ffd96ad5b199a533b4ce0c2f6573ac7c08c33
Isabella Cascais <isalelis31@gmail.com>
2025-03-23 03:41:33 -0300

    Implementação funcão para escolher serviço

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,18 @@
 function navegar(destino) {
     window.location.href = destino;
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const servicos = document.querySelectorAll("input[name='servico']");
+    const opcaoDayCare = document.getElementById("opcao-daycare");
+
+    servicos.forEach(servico => {
+        servico.addEventListener("change", function() {
+            if (this.value === "daycare") {
+                opcaoDayCare.style.display = "block";
+            } else {
+                opcaoDayCare.style.display = "none";
+            }
+        });
+    });
+});
